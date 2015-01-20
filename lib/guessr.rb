18ab08a2ac78798674f28493e2563cdf595ae1d7
Brit Butler <redline6561@gmail.com>
2015-01-20 00:26:03 -0500

    Scaffold out functions for game loop.

--- lib/guessr.rb
+++ lib/guessr.rb
@@ -59,3 +59,59 @@ end
 def Guessr.create
   Guessr::Models.create_schema
 end
+
+def choose_player
+  # TODO
+end
+
+def new_player
+  # TODO
+end
+
+def resume_game
+  # TODO
+end
+
+def new_game
+  # TODO
+end
+
+def prompt(question, validator, error_msg, clear: nil)
+  `clear` if clear
+  puts "\n#{question}\n"
+  result = gets.chomp
+  until result =~ validator
+    puts "\n#{error_msg}\n"
+    result = gets.chomp
+  end
+  exit if result == 'QUIT'
+  puts
+  result
+end
+
+def player_screen
+  message = "Would you like to (1) pick an existing player, (2) add a new player, or (QUIT)?"
+  choice = prompt(message, /^([12]|QUIT)$/, "Please choose 1, 2, 3, or QUIT.", clear: true).to_i
+  case choice
+  when 1
+    choose_player
+  when 2
+    new_player
+  end
+end
+
+def game_screen
+  message = "Would you like to (1) continue an existing game, (2) start a new game, or (QUIT)?"
+  choice = prompt(message, /^([12]|QUIT)$/, "Please choose 1, 2, or QUIT.", clear: true).to_i
+  case choice
+  when 1
+    resume_game
+  when 2
+    new_game
+  end
+end
+
+puts "Welcome to Guessr!"
+player_screen
+game_screen
+puts "Thanks for playing!"
