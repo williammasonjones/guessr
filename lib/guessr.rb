2091bc397364da4422a6a1217b34cf1dbcc73a84
Brit Butler <redline6561@gmail.com>
2015-01-19 22:45:33 -0500

    Add some models.

--- lib/guessr.rb
+++ lib/guessr.rb
@@ -1,5 +1,39 @@
 require "guessr/version"
 
+Camping.goes :Guessr
+
 module Guessr
-  # Your code goes here...
+  module Models
+    class Player < Base
+    end
+
+    class Hangman < Base
+    end
+
+    class BasicSchema < V 1.0
+      def self.up
+        create_table Player.table_name do |t|
+          t.string :name
+          t.timestamps
+        end
+
+        create_table Hangman.table_name do |t|
+          t.integer :turns, :default => 7
+          t.string :answer
+          t.string :guesses
+          t.boolean :finished
+          t.timestamps
+        end
+      end
+
+      def self.down
+        drop_table Player.table_name
+        drop_table Hangman.table_name
+      end
+    end
+  end
+end
+
+def Guessr.create
+  Guessr::Models.create_schema
 end
--- test/first_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'test_helper'
-
-class FirstTest < Minitest::Test
-  def test_we_have_the_version
-    assert_equal Guessr::VERSION, '0.0.1'
-  end
-end
